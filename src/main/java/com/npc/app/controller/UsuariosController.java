cb34c26a0e837f371fa6de7cc5a8eb10f33fc9ed
AbraaoAlves <abraao.teodosio@gmail.com>
2023-09-25 02:33:06 +0000

    config: integrate UI app and Java build
    - build, test and deploy UI app on build Java app

--- src/main/java/com/npc/app/controller/UsuariosController.java
+++ src/main/java/com/npc/app/controller/UsuariosController.java
@@ -1,13 +1,11 @@
 package com.npc.app.controller;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:8080")
