85996f16df432ab762cd344c729fb6c41e2c9479
AbraaoAlves <abraao.teodosio@gmail.com>
2023-09-27 10:59:00 +0000

    config: add proxy to dev env

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,6 +6,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
   plugins: [react()],
   test: {
     globals: true,
