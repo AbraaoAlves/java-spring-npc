583378049acdb41e12171e9635ef6b51d85553cd
AbraaoAlves <abraao.teodosio@gmail.com>
2023-10-04 04:08:45 +0000

    fix: load java page type

--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 export type Usuario = {
   id: string;
@@ -7,25 +7,47 @@ export type Usuario = {
   password?: string;
   createdAt?: string;
   updatedAt?: string;
+};
+
+export async function getUsuarios() {
+  return axios.get<JavaPage<Usuario>>("/api/users");
 }
 
-export async function getUsuarios(){
-  return axios.get<Array<Usuario>>('/api/users');  
+export async function getUsuario(id: string) {
+  return axios.get<Usuario>("/api/users/" + id);
 }
 
-export async function getUsuario(id: string){
-  return axios.get<Usuario>('/api/users/' + id)  
+export async function createUsuario(user: Partial<Omit<Usuario, "id">>) {
+  return axios.post<Usuario>("/api/users/", user);
 }
 
-export async function createUsuario(user: Partial<Omit<Usuario, 'id'>>){
-  return axios.post<Usuario>('/api/users/', user);
+export async function updateUsuario(
+  id: string,
+  user: Partial<Omit<Usuario, "id">>
+) {
+  return axios.put<Usuario>("/api/users/" + id, user);
 }
 
-
-export async function updateUsuario(id: string, user: Partial<Omit<Usuario, 'id'>>){
-  return axios.put<Usuario>('/api/users/'+id, user);
+export async function deleteUsuario(id: string) {
+  await axios.delete<Usuario>("/api/users/" + id);
 }
 
-export async function deleteUsuario(id: string){
-  await axios.delete<Usuario>('/api/users/'+id);
-}
+export type JavaPage<T> = {
+  content: Array<T>,
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: { empty: boolean; sorted: boolean; unsorted: boolean };
+    offset: number;
+    unpaged: boolean;
+  };
+  size: number;
+  sort: { empty: boolean; sorted: boolean; unsorted: boolean };
+  totalElements: number;
+  totalPages: number;
+};
