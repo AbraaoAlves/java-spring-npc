b4d7960345ab460207e5ce5c6eb5277dd98428d5
AbraaoAlves <abraao.teodosio@gmail.com>
2023-10-04 04:18:39 +0000

    styles: app bar and drawer

--- ui/src/pages/Layout.tsx
+++ ui/src/pages/Layout.tsx
@@ -1,56 +1,81 @@
 import { Form, useNavigation, useLoaderData, redirect } from "react-router-dom";
 import { UserList, UserListItem } from "../components/UserList/UserList";
 import ActiveNavLink from "../components/ActiveNavlink";
+import { AppBar } from "../components/AppBar/AppBar";
 import { Outlet } from "@mui/icons-material";
-import { createUsuario, getUsuarios, type Usuario } from "../api";
+import { createUsuario, getUsuarios, JavaPage, type Usuario } from "../api";
+import "./Layout.css";
+import Drawer from "@mui/material/Drawer";
+import { Box, Toolbar } from "@mui/material";
 
 Layout.loader = async () => getUsuarios();
 Layout.action = async () => {
-  const {data} = await createUsuario({name: 'novo usuario'});
-  
-  return redirect(`/users/${data.id}/edit`);
-}
-export default function Layout() {
+  const { data } = await createUsuario({ name: "novo usuario" });
 
-  const {data: usuarios} = useLoaderData() as {data: Array<Usuario> | null};
+  return redirect(`/users/${data.id}/edit`);
+};
+export default function Layout() {
+  const { data } = useLoaderData() as { data: JavaPage<Usuario> | null };
   const navigation = useNavigation();
+  const usuarios = data?.content;
 
   return (
     <>
-      <div id="sidebar">
-        <h1>UECE NPC APP: </h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div id="search-spinner" aria-hidden hidden={true} />
-            <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <Form method="post">
-            <button type="submit">Novo</button>
-          </Form>
+      <AppBar />
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: 340,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: 340, boxSizing: "border-box" },
+        }}
+      >
+        <Toolbar />
+        <div id="sidebar">
+          <div>
+            <form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div id="search-spinner" aria-hidden hidden={true} />
+              <div className="sr-only" aria-live="polite"></div>
+            </form>
+            <Form method="post">
+              <button type="submit">Novo</button>
+            </Form>
+          </div>
+          <nav>
+            {usuarios?.length ? (
+              <UserList>
+                {usuarios.map(({ id, name, email }) => (
+                  <ActiveNavLink to={`users/${id}`}>
+                    <UserListItem name={name} email={email} />
+                  </ActiveNavLink>
+                ))}
+              </UserList>
+            ) : (
+              <p>
+                <i>Sem usuarios</i>
+              </p>
+            )}
+          </nav>
+
+          <Box
+            sx={{
+              position: "fixed",
+              bottom: 0,
+              textAlign: "center",
+              pl: 5,
+            }}
+          >
+            <h4>UECE NPC APP! 👨🏿‍💻👩‍💻👩🏻‍💻👩🏼‍💻</h4>
+          </Box>
         </div>
-        <nav>
-          {usuarios?.length ? (
-            <UserList>
-              {usuarios.map(({ id, name, email }) => (
-                <ActiveNavLink to={`users/${id}`}>
-                  <UserListItem name={name} email={email} />
-                </ActiveNavLink>
-              ))}
-            </UserList>
-          ) : (
-            <p>
-              <i>Sem usuarios</i>
-            </p>
-          )}
-        </nav>
-      </div>
+      </Drawer>
       <div
         id="detail"
         className={navigation.state === "loading" ? "loading" : ""}
@@ -60,6 +85,3 @@ export default function Layout() {
     </>
   );
 }
-
-
-
