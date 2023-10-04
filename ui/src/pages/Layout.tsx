c2d1e17440d0aebce2fb3a2e8a29c8c48e4a3ec5
AbraaoAlves <abraao.teodosio@gmail.com>
2023-10-04 01:53:59 +0000

    feat: implement first apperence of page

--- ui/src/pages/Layout.tsx
+++ ui/src/pages/Layout.tsx
@@ -1,8 +1,45 @@
+import { Form } from "react-router-dom";
+import { UserList, UserListItem } from "../components/UserList/UserList";
+import ActiveNavLink from "../components/ActiveNavlink";
+import { Usuario } from "../api";
 
-
-
-export default function Layout(){
-  return <></>
+export default function Layout() {
+  const usuarios = [] as Array<Usuario>;
+  
+  return (
+    <div id="sidebar">
+      <h1>UECE NPC APP: </h1>
+      <div>
+        <form id="search-form" role="search">
+          <input
+            id="q"
+            aria-label="Search contacts"
+            placeholder="Search"
+            type="search"
+            name="q"
+          />
+          <div id="search-spinner" aria-hidden hidden={true} />
+          <div className="sr-only" aria-live="polite"></div>
+        </form>
+        <Form method="post">
+          <button type="submit">Novo</button>
+        </Form>
+      </div>
+      <nav>
+        <UserList>
+          {usuarios.map(({id, name, email}) => 
+            <ActiveNavLink to={`users/${id}`}>
+              <UserListItem name={name} email={email} />
+            </ActiveNavLink>
+          )}
+        </UserList>
+      </nav>
+    </div>
+  );
 }
 
-Layout.otherProps = '';
+
+
+
+
+Layout.otherProps = "";
