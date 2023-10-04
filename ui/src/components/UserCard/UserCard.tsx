509417ea22e019a83b2e4ca02e0f0c6bac9b9276
AbraaoAlves <abraao.teodosio@gmail.com>
2023-10-04 17:20:56 +0000

    feat: add day js

--- ui/src/components/UserCard/UserCard.tsx
+++ ui/src/components/UserCard/UserCard.tsx
@@ -7,47 +7,70 @@ import {
   Typography,
   Divider,
   Grid,
+  Box,
 } from "@mui/material";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 type Props = {
   id: string;
   name: string;
   email: string;
   createdAt?: string;
-  updateAt?: string;
-  children: React.ReactNode; 
+  updatedAt?: string;
+  children: React.ReactNode;
 };
 
 export const UserCard: React.FC<Props> = (props) => {
+  const createdAt = dayjs(props.createdAt ?? "").fromNow();
+  const updatedAt = dayjs(props.updatedAt ?? "").fromNow();
+
   return (
-    <Card>
-      <Grid container>
-        <Grid item>
-          <CardMedia component="img" height={180} src={fakeImgPrefix(props)} />
+    <Card sx={{maxWidth:'80%'}}>
+      <Grid container >
+        <Grid item md={6} xs={12}>
+          <CardMedia component="img" height="100%"  src={fakeImgPrefix(props)} />
         </Grid>
-        <Grid item xs>
-          <CardContent >
-            <Typography variant="h5">{props.name}</Typography>
-            <Typography variant="body1" color="text.secondary" component="div" gutterBottom>
+        <Grid
+          item
+          xs
+          sm
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <CardContent>
+            <Typography variant="h4">{props.name}</Typography>
+            <Typography
+              variant="body1"
+              color="text.secondary"
+              component="div"
+              gutterBottom
+            >
               {props.email}
             </Typography>
-
-            <Divider variant="fullWidth" />
-
-
-            <i>
-              Criado em: {props.createdAt ?? ""}
-            </i>
-
-            <i>
-              Atualizado em: {props.updateAt ?? ""}
-            </i>
-
           </CardContent>
-          <CardActions>
-            {props.children}
+          
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end'}}>
+            <Box textAlign="end" sx={{ mr: 1 }}>
+              <Typography variant="body2" color="text.secondary">
+                Created at <i>{createdAt}</i>
+              </Typography>
+              <Typography variant="body2" color="text.secondary" gutterBottom>
+                Aupdated at <i>{updatedAt}</i>
+              </Typography>
+              <Divider variant="fullWidth" sx={{ marginTop: "5%" }} />
+            </Box>
 
-          </CardActions>
+            <CardActions sx={{ justifyContent: "flex-end" }}>
+              {props.children}
+            </CardActions>
+          </Box>
         </Grid>
       </Grid>
     </Card>
