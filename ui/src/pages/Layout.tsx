915dc3858b3ef57b7ef167c6c6496c550ff5b210
AbraaoAlves <abraao.teodosio@gmail.com>
2023-10-04 01:59:46 +0000

    chore: improve list of users

--- ui/src/pages/Layout.tsx
+++ ui/src/pages/Layout.tsx
@@ -1,45 +1,61 @@
-import { Form } from "react-router-dom";
+import { Form, useNavigation, useLoaderData } from "react-router-dom";
 import { UserList, UserListItem } from "../components/UserList/UserList";
 import ActiveNavLink from "../components/ActiveNavlink";
-import { Usuario } from "../api";
+import { Outlet } from "@mui/icons-material";
+import type { Usuario } from "../api";
+
+
 
 export default function Layout() {
-  const usuarios = [] as Array<Usuario>;
-  
+
+  const usuarios = useLoaderData() as Array<Usuario>;
+  const navigation = useNavigation();
+
   return (
-    <div id="sidebar">
-      <h1>UECE NPC APP: </h1>
-      <div>
-        <form id="search-form" role="search">
-          <input
-            id="q"
-            aria-label="Search contacts"
-            placeholder="Search"
-            type="search"
-            name="q"
-          />
-          <div id="search-spinner" aria-hidden hidden={true} />
-          <div className="sr-only" aria-live="polite"></div>
-        </form>
-        <Form method="post">
-          <button type="submit">Novo</button>
-        </Form>
-      </div>
-      <nav>
-        <UserList>
-          {usuarios.map(({id, name, email}) => 
-            <ActiveNavLink to={`users/${id}`}>
-              <UserListItem name={name} email={email} />
-            </ActiveNavLink>
+    <>
+      <div id="sidebar">
+        <h1>UECE NPC APP: </h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div id="search-spinner" aria-hidden hidden={true} />
+            <div className="sr-only" aria-live="polite"></div>
+          </form>
+          <Form method="post">
+            <button type="submit">Novo</button>
+          </Form>
+        </div>
+        <nav>
+          {usuarios?.length ? (
+            <UserList>
+              {usuarios.map(({ id, name, email }) => (
+                <ActiveNavLink to={`users/${id}`}>
+                  <UserListItem name={name} email={email} />
+                </ActiveNavLink>
+              ))}
+            </UserList>
+          ) : (
+            <p>
+              <i>Sem usuarios</i>
+            </p>
           )}
-        </UserList>
-      </nav>
-    </div>
+        </nav>
+      </div>
+      <div
+        id="detail"
+        className={navigation.state === "loading" ? "loading" : ""}
+      >
+        <Outlet />
+      </div>
+    </>
   );
 }
 
-
-
-
-
 Layout.otherProps = "";
+
