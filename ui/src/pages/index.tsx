31c07f1130fddeddd6e7731551022d83a3e4c89f
Kilvia Santos <58449533+KilviaSantos@users.noreply.github.com>
2023-10-06 01:21:46 +0000

    feat: usnado novo componente de background no index

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,19 +1,27 @@
-import imgUrl from '../assets/pexels-photo-583847.jpeg'
+import { Typography } from "@mui/material";
+import imgUrl from "../assets/pexels-photo-583847.jpeg";
+import { BackGroundContainer } from "../components/BackgroundContainer";
 
-export default function  Index() {
-  return (<div style={{background: 'url(' + imgUrl + ')'}}>
-    <p id="zero-state">
-      <h1>GESTÃO DE USUÁRIOS</h1>
-      <br />
-      <br />
-      <br />
-      Veja nosso codigo em {" "}
-      <a href="https://github.com/AbraaoAlves/java-spring-npc/">
-        npc project
-      </a>
-      .
-    </p>
+export default function Index() {
+  return (
+    <BackGroundContainer
+      imgUrl={imgUrl}
+      imgOpacity={.2}
+      height="90vh"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+      }}
+    >
+      <Typography variant="h3">GESTÃO DE USUÁRIOS</Typography>
 
-  </div>
+      <Typography variant="body1">
+        Veja nosso codigo em{" "}
+        <a href="https://github.com/AbraaoAlves/java-spring-npc/">
+          npc project
+        </a>
+      </Typography>
+    </BackGroundContainer>
   );
 }
